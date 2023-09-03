f4ed4158f87c4f95ae31f4aac166006e698f1be8
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-03 11:35:37 +0100

    added csv file, updated docstring

--- ANN_Classification.py
+++ ANN_Classification.py
@@ -4,6 +4,13 @@ In this module a classification task is performed with TensorFlow.
 This inclueds identifying and dealing with overfitting through Early Stopping Callbacks 
 and Dropout Layers.
 
+Early Stopping - Keras can stop training based on a loss condition on the validation data
+passed during the model.fit() call.
+
+Dropout Layers - Dropout layer can be added to layers to 'turn off' neurons to prevent
+overfitting.  Each Dropout Layer will turn off a user-defined percentage of neurons in the
+previous layer with each batch of data. 
+
 """
 
 import pandas as pd
@@ -15,3 +22,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import (mean_absolute_error, mean_squared_error, 
             mean_absolute_percentage_error, explained_variance_score)
 from tensorflow.keras.models import Sequential
+
+df = pd.read_csv('cancer_classification.csv')
+
+
