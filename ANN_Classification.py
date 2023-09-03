5bf9fd4730966a8a4145620be669b2ef2a5091a7
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-03 12:01:14 +0100

    data processing completed

--- ANN_Classification.py
+++ ANN_Classification.py
@@ -25,4 +25,39 @@ from tensorflow.keras.models import Sequential
 
 df = pd.read_csv('cancer_classification.csv')
 
+########################################################
+# Data Exploration
+########################################################
+print(df.info())
+print('\n There are no null values.')
+print(df.describe().transpose())
+
+#Is the label class well balanced?
+sns.countplot(x='benign_0__mal_1', data=df)
+plt.show()
+
+#Let's see if the label class is highly correlated with its features
+print('\n Lets see if the label class is highly correlated with its features')
+print(df.corr()['benign_0__mal_1'].sort_values())
+#Lets look at a plot of this data. Exclude the last classification column
+plt.figure(figsize=(12,6))
+df.corr()['benign_0__mal_1'][:-1].sort_values().plot(kind='bar')
+plt.show()
+
+#############################################################
+# Data Preparation
+#############################################################
+#Get numpy arrays of data
+X = df.drop('benign_0__mal_1', axes=1).values  #Drop label column from training data
+y = df['benign_0__mal_1'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=101)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
+
 
