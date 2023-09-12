f8ddf54bc3f9e355f9b97c1ad55f354f80907f24
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-12 01:35:04 +0100

    completed model evaluation

--- CNN_Fashion_MNIST.py
+++ CNN_Fashion_MNIST.py
@@ -4,10 +4,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Dropout, Flatten
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from matplotlib import image as mpimg
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
+import pandas as pd
 
 ###############################################################
 ### Load the data
@@ -54,4 +56,22 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['ac
 #################################################################
 # Train the model
 #################################################################
-model.fit(x_train, y_cat_train, epochs=10, validation_data=(x_test, y_cat_test))
+model.fit(x_train, y_cat_train, epochs=5, validation_data=(x_test, y_cat_test))
+
+#################################################################
+# Evaluate the model
+#################################################################
+# In this section the accuracy, precision, recall, f1-score the model achieved 
+# on the x_test data set will be evaluated
+
+metrics = pd.DataFrame(model.history.history)
+print('metrics = ', metrics)
+
+metrics[['loss', 'val_loss']].plot()
+metrics[['accuracy', 'val_accuracy']].plot()
+plt.show()
+
+y_pred = model.predict(x_test)
+predicted_labels = np.argmax(y_pred, axis=1)
+print('\n predicted_labels = {}'.format(predicted_labels))
+print('\n classification_report = ', classification_report(y_test, predicted_labels))
