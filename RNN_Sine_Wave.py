4f1eb1c0bdaab9cf78df06f1188a8d1663dece36
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-13 11:11:53 +0100

    Set up data

--- RNN_Sine_Wave.py
+++ RNN_Sine_Wave.py
@@ -4,21 +4,33 @@ import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import MinMaxScaler
 
+#####################################################
 # Create sine wave data
-x = np.linspace(0, 50, 501)
+#####################################################
+x = np.linspace(0, 50, 501)  #Returns 501 evenly spaced numbers over range 0->50
 y = np.sin(x)
 
 plt.plot(x,y)
-plt.show()
+plt.show()  #Shows a sine wave from 0 to 50
 
-df =pd.DataFrame(data=y, index=x, columns=['Sine'])
+df = pd.DataFrame(data=y, index=x, columns=['Sine'])  #column in sine of index value, x
 
-test_percent = 0.1
-test_point = np.round(len(df)*test_percent)
-test_ind = int(len(df) - test_point)
+test_percent = 0.1 #10% of values at the end of the data is the test set
+test_point = np.round(len(df) * test_percent)
+test_index = int(len(df) - test_point) #test data are beyond this index
 
-train = df.iloc[:test_ind]
-test = df.iloc[test_ind:]
+#iloc = index location slicing
+train = df.iloc[:test_index] #slice data from start of the array upto test_index 
+test = df.iloc[test_index:]  #slice data from test_index to end of array
+
+# scale the data. Need to scale the y, label data as it is  
+# fed back into the neurons.
+scaler = MinMaxScaler()
+scaler.fit(train)  #fit to training data
+scaled_train = scaler.transform(train)
+scaled_test = scaler.transform(test) #Only transform test data, do not fit
+
+
 
 
     
