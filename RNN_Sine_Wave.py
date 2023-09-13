0bbaf58ad7b6aac7fb544333c0ea7a356e93f587
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-13 11:29:02 +0100

    TimeseriesGenerater added

--- RNN_Sine_Wave.py
+++ RNN_Sine_Wave.py
@@ -1,7 +1,29 @@
+"""
+In this module a simple Recurrent Neural Network is created that predicts
+values in a sine curve time sequence.
+
+TimeseriesGenerator is used to generate time series data samples
+
+TimeseriesGenerator is a utility class provided by TensorFlow's Keras API (tf.keras)
+for generating time series data samples from a given sequence of data. 
+It's particularly useful when working with time series forecasting 
+and sequence prediction tasks. 
+The primary function of TimeseriesGenerator is to create batches of 
+input-output pairs from a given sequence of data, allowing you to 
+feed this data into a machine learning model for training or evaluation.
+
+TimeseriesGenerator is particularly useful when you have a large 
+time series dataset and want to efficiently generate batches of data 
+for training recurrent neural networks (RNNs), convolutional neural networks (CNNs),
+or any other type of sequence-based model. 
+It simplifies the process of handling time series data and ensures 
+that your model receives input data in the appropriate format.
+
+"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 from sklearn.preprocessing import MinMaxScaler
 
 #####################################################
@@ -30,7 +52,13 @@ scaler.fit(train)  #fit to training data
 scaled_train = scaler.transform(train)
 scaled_test = scaler.transform(test) #Only transform test data, do not fit
 
-
+########################################################
+# TimeseriesGenerator
+########################################################
+length = 25  #Feed model 25 data items and ask it to predict the 26th data item
+batch_size = 1
+generator = TimeseriesGenerator(scaled_train, scaled_train,
+                                length=length, batch_size=batch_size)
 
 
     
