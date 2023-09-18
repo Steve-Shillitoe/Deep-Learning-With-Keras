ee2946a075eb84f87f4285752a7eb8aa28ebe304
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-18 23:05:35 +0100

    deleted file as it is no longer used

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-content = {"sepal_length":5.1,"sepal_width":3.5,"petal_length":1.4,"petal_width":0.2}
-
-result = requests.post("http://127.0.0.1:5000/api/flower", json=content)
-
-print(result.status_code)
-print(result.text)
